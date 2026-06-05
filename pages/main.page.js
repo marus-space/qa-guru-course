e8062c3711ea52cb931de67ec8eb7a4e098426ee
Maria Gopkalo <marus.space@gmail.com>
2026-06-05 21:41:05 +0300

    patterns lesson

--- pages/main.page.js
+++ pages/main.page.js
@@ -1,6 +1,12 @@
 export class MainPage {
   constructor({ page }) {
+    this.page = page;
+    this.baseUrl = 'https://realworld.qa.guru/';
     this.signUpLink = page.getByRole('link', { name: 'Sign up' });
+  }
+
+  async goToMainPage() { 
+    await this.page.goto(this.baseUrl);
   }
 
   async goToRegister() {
